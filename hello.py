6bb241cadc0abfe9a0b8b5a9d4ad3ffe1ff4cdc3
Bruce Elgort <belgort@clark.edu>
2021-09-25 18:29:06 -0700

    Initial commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+# hello.py
+"""
+CTEC 121
+Date: <ex: mm/dd/yyyy>
+<your name>
+Name: <assignment/lab name>
+Description: <assignment/lab description>
+"""
+
+def main():
+    print()
+    print("Hello")
+    print()
+
+
+main()    
