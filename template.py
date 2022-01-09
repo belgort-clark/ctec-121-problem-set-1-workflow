b8eba296e4ce24f3eeb403a2097511ca53337eec
Bruce Elgort <bruce.elgort@gmail.com>
2022-01-09 10:15:20 -0800

    Updated for Winter 2022

---
+++ template.py
@@ -0,0 +1,20 @@
+"""
+CTEC 121
+Date: mm/dd/yyyy
+YOUR NAME
+Name: Problem Set 1 - Assignment Workflow
+Description: To demonstrate how the assignment workflow works
+"""
+
+"""
+IPO (Inputs, Processes and Outputs)
+Inputs: 
+Processes: 
+Outputs: 
+"""
+
+def main():
+    # your code goes here (remove this line)
+
+
+main()    
