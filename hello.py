18ab963cf1e65309bbe66f160e9296227d396f47
Bruce Elgort <bruce.elgort@gmail.com>
2022-01-09 13:09:53 -0800

    Updated for Winter 2022

---
+++ hello.py
@@ -0,0 +1,21 @@
+"""
+CTEC 121
+Date: mm/dd/yyyy
+YOUR NAME
+Name: Problem Set 1 - Assignment Workflow
+Description: To demonstrate how the assignment workflow works
+"""
+
+"""
+IPO (Inputs, Processes and Outputs)
+Inputs: No inputs
+Processes: No processes
+Outputs: Print 3 messages
+"""
+
+def main():
+    print("This is Problem Set No. 1!")
+    # Add at least 2 additional print statements that print whatever you want. Be sure to remove this comment.
+
+
+main()    
